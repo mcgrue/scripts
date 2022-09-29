d94f4200fe55aaf2b6b87a8599e287d688027aa9
Ben McGraw <ben.mcgraw@gmail.com>
2022-09-29 16:00:40 -0700

    branchtime!

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,3 +1,5 @@
 
 git config --global alias.unadd "reset HEAD"
 git config --global alias.uncommit "reset --soft HEAD^"
+
+alias branchtime='for k in $(git branch | perl -pe s/^..//); do echo -e $(git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1)\\t$k; done | sort -r'
