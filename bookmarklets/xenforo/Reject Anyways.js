fd9a0f51701ef8bd9434955891f29a37eda65975
Ben McGraw <ben.mcgraw@gmail.com>
2023-11-17 20:59:05 -0800

    Make bookmarklet only work on correct page or redirect you there

--- bookmarklets/xenforo/Reject Anyways.js
+++ bookmarklets/xenforo/Reject Anyways.js
@@ -1,1 +1,26 @@
-javascript:(function() {  function clickSubmitIfNothingToDo() { var submitButton = document.querySelector('input[type="submit"][value="Process Users"]');  if (submitButton) {     submitButton.click(); } else {     alert("Submit button with the value 'Process Users' not found."); } }  clickSubmitIfNothingToDo();  })();
+
+javascript:(function() {  
+
+    function correctPageValidator() {
+        var expectedURL = 'https://community.gaslampgames.com/admin.php?users/moderated';
+        var currentURL = window.location.href;
+        
+        if (currentURL !== expectedURL) {
+            alert('You are on the wrong page. You will be redirected to the correct page.');
+            window.location.href = expectedURL;
+        } else {
+            return true;
+        }
+    };
+
+    function clickSubmitIfNothingToDo() { 
+        var submitButton = document.querySelector('input[type="submit"][value="Process Users"]');  
+        if (submitButton) {     submitButton.click(); } 
+        else {     alert("Submit button with the value 'Process Users' not found."); }
+    }  
+
+    if(correctPageValidator())
+    {
+        clickSubmitIfNothingToDo();      
+    }
+})();
