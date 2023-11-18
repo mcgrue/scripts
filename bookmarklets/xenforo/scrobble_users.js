2d63a852be26e22b1fca539478ffdc850fde1a3a
Ben McGraw <ben.mcgraw@gmail.com>
2023-11-17 20:43:11 -0800

    Create scrobble_users.js
    This is a bookmarklet to quickly scrub through spam accounts and filter for "ham"

---
+++ bookmarklets/xenforo/scrobble_users.js
@@ -0,0 +1,87 @@
+
+javascript:(function() {
+
+	function rejectEverythingByDefault() {
+		/* set everything to reject by default */
+	    var radioButtons = document.querySelectorAll('.Disabler[type="radio"]');
+	    for (var i = 0; i < radioButtons.length; i++) {
+	        radioButtons[i].checked = true;
+	    }
+	}
+
+    function uncheckAllNotificationsByDefault() {
+	    /* get a set of all the labels containing this text */
+		var labelText = "Notify user if action was taken";
+		var labelElements = Array.from(document.querySelectorAll('label')).filter(function(label) {
+		    return label.textContent.includes(labelText);
+		});
+		/* uncheck them all */
+		for (var i = 0; i < labelElements.length; i++) {
+		    var label = labelElements[i];
+		    var checkbox = label.querySelector('input[type="checkbox"]');
+
+		    if (checkbox !== null) {
+		        checkbox.checked = false;
+		    }
+		}
+	}
+
+	function collectAllPotentiallyValidEmails() {
+		var whitelistedEmailProviders = ['@gmail.', '@hotmail.', '@outlook.', '@apple.', '@yahoo.', '@msn.', '@protonmail.', '@mail.'];
+
+		var ddElements = document.querySelectorAll('dd');
+
+		var matchingTextContent = [];
+
+		for (var i = 0; i < ddElements.length; i++) {
+		    var dd = ddElements[i];
+		    var text = dd.textContent;
+
+		    if (whitelistedEmailProviders.some(function(literal) {
+		        return text.includes(literal);
+		    })) {
+		        matchingTextContent.push(text.trim());
+		    }
+		}
+
+		return matchingTextContent;
+	}
+
+	function filterEmailsForTooManyDots(inputArray) {
+		var filteredArray = inputArray.filter(function(entry) {
+		    var email = entry.match(/\S+@\S+/);
+
+		    if (email) {
+		        var username = email[0].split('@')[0];
+		        var periodCount = (username.match(/\./g) || []).length;
+		        return periodCount <= 2;
+		    } else {
+		        return true;
+		    }
+		});
+
+		return filteredArray;
+	}
+
+	function clickSubmitIfNothingToDo() {
+		var submitButton = document.querySelector('input[type="submit"][value="Process Users"]');
+
+		if (submitButton) {
+		    submitButton.click();
+		} else {
+		    alert("Submit button with the value 'Process Users' not found.");
+		}
+	}
+
+
+	rejectEverythingByDefault();
+	uncheckAllNotificationsByDefault();
+	var realWork = filterEmailsForTooManyDots(collectAllPotentiallyValidEmails());
+
+	if(realWork.length == 0) {
+		clickSubmitIfNothingToDo();
+	} else {
+		var msg = "Please manually review these emails: \n\n" + realWork.join('\n');
+		alert(msg);
+	}
+})();
