3e2a3ff60708b63ca2e13a5c43a64465b047c28d
Grue <ben.mcgraw@gmail.com>
2026-06-04 14:57:33 -0700

    Update git aliases to use git config commands

--- git_aliases.sh
+++ git_aliases.sh
@@ -5,10 +5,10 @@ git config --global alias.uncommit "reset --soft HEAD^"
 git config --global alias.list-local-branches "for-each-ref --sort='-committerdate' --format='%(committerdate:short) %(refname:short)' refs/heads/"
 git config --global alias.list-upstream-branches "for-each-ref --sort='-committerdate' --format='%(committerdate:short) %(refname:short)' refs/remotes/"
 git config --global alias.recent "for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short) %(committerdate:relative)' --count=5"
-alias co='git checkout'
-alias b='git branch -v'
-alias s='git status'
-alias d='git branch -D'
-alias p='git pull --quiet --no-stat'
+git config --global alias.co checkout
+git config --global alias.b 'branch -v'
+git config --global alias.s status
+git config --global alias.d 'branch -D'
+git config --global alias.p 'pull --quiet --no-stat'
 
 alias branchtime='for k in $(git branch | perl -pe s/^..//); do echo -e $(git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1)\\t$k; done | sort -r'
