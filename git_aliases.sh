257f52b1997a8e7dc30db840ed9998795cbe994c
Grue <ben.mcgraw@gmail.com>
2026-07-13 14:26:32 -0700

    Update git_aliases.sh

--- git_aliases.sh
+++ git_aliases.sh
@@ -10,5 +10,6 @@ git config --global alias.b 'branch -v'
 git config --global alias.s status
 git config --global alias.d 'branch -D'
 git config --global alias.p 'pull --quiet --no-stat'
+git config --global alias.nb 'nb = "!f() { DATE=$(date +%Y-%m-%d); NAME=$(git config user.name | tr -d \" \" | tr A-Z a-z); git checkout -b $NAME/$DATE/$1; }; f"'
 
 alias branchtime='for k in $(git branch | perl -pe s/^..//); do echo -e $(git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | head -n 1)\\t$k; done | sort -r'
